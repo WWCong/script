fe0705f38db5619857013ee7847eca8ad4524c59
unknown <>
2016-02-17 17:54:15 +0800

    if

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 (function($){ 
-    $('body').append("<div id='wc'></div>");
+    if (document.getElementById('wc') == null)
+        $('body').append("<div id='wc'></div>");
     $('#wc').load('https://rawgit.com/WWCong/script/master/test.html .wc-load')
 })(jQuery);
