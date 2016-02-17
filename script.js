38df62d49c530ca1e6fbd94197260d8811cdc8eb
unknown <>
2016-02-17 15:19:08 +0800

    aaa

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-
-var figures = workflow.getFigures();
-var prop = new Map();
-figures.data.forEach(function(figure) {
-    if (figure != null && figure != undefined) {
-        for(var item in (figure) )
-        {
-            prop.set(item, 1);
+(function($){ 
+    var figures = workflow.getFigures();
+    var prop = new Map();
+    figures.data.forEach(function(figure) {
+        if (figure != null && figure != undefined) {
+            for(var item in (figure) )
+            {
+                prop.set(item, 1);
+            }
         }
+    });
+    var str = "";
+    for (var k in prop.keys) {
+        str += k + '\n';
     }
-});
-var str = "";
-for (var k in prop.keys) {
-    str += k + '\n';
-}
-alert(str);
+    alert(str);
 
-var figure = workflow.getFigure("8cd04028-8e6b-b56e-6863-35192a943ce2");
+    var figure = workflow.getFigure("8cd04028-8e6b-b56e-6863-35192a943ce2");
+})(jQuery);
