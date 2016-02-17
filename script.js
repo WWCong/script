8f37b3a3970e5638ae04617063fc54bdb74819a4
unknown <>
2016-02-17 18:20:20 +0800

    aa

--- script.js
+++ script.js
@@ -3,4 +3,10 @@
         $('body').append("<div id='wc'></div>");
     //$('#wc').load('https://rawgit.com/WWCong/script/master/test.html .wc-load')
     $('#wc').append("<div style='position: fixed;height: 80%;width: 20%;background-color: white;top: 20px;right: 0px;border-left: 1px solid #ccc;padding: 10px;'><p>name</p><p id='wc-name'></p><p>pkgCpt</p><p id='wc-pkg'></p><p>Output</p><p id='wc-output'></p></div>");
+    
+    $('#mainContents').click(function(){
+        var figure = workflow.getCurrentSelection();
+        $('#wc-name').html(figure.name);
+        $('#wc-pkg').html(figure.pkgCapt);
+    });
 })(jQuery);
