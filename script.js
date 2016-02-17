c99201264e2d7b0cbf638df2df31640597d7a84a
unknown <>
2016-02-17 17:24:37 +0800

    UI V1.0

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 (function($){ 
-    var figures = workflow.getFigures();
-    var prop = new Map();
-    var counter = 0;
-    figures.data.forEach(function(figure) {
-        if (figure != null && figure != undefined) {
-            for(var item in (figure) )
-            {
-                alert(item);
-                prop.set(item, 1);
-                if (counter++ == 20)
-                    return;
-            }
-        }
+    //$('body').append('<div style="position:fixed;height:100px;weight:100px;background-color:red;">12312</div>');
+    $('body').click(function () {
+        alert("click");
     });
-    var str = "";
-    for (var k in prop.keys) {
-        str += k + '\n';
-    }
-    alert(str);
-
-    var figure = workflow.getFigure("8cd04028-8e6b-b56e-6863-35192a943ce2");
 })(jQuery);
+
+
+var prop = new Map();
+var counter = 0;
+var figures = workflow.getFigures();
+var arr = [];
+figures.data.forEach(function(figure) {
+    if (figure != null && figure != undefined) {
+        arr.push(figure.pkgCapt + ", " + figure.name)
+    }
+});
+console.log(arr.join('\n'));
+var str = "";
+for (var k in prop.keys) {
+    str += k + '\n';
+}
+console.log(str);
+
+var figure = workflow.getCurrentSelection();
+alert(figure);
+alert(figure.pkgCapt + ", " + figure.name);
